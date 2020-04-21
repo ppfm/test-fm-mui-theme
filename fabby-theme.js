e02d5fc63ae2011f19b7adf22241669c3e5ebaf4
ppfm <fm@psychpress.com.au>
2020-04-21 15:22:50 +1000

    testing simple theme package

---
+++ fabby-theme.js
@@ -0,0 +1,10 @@
+import {createMuiTheme} from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#94cca7',
+        }
+    }
+});
+export default theme;

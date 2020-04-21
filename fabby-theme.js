f06875fe167e33ba00bd45baca6ff9d0b00ef88c
ppfm <fm@psychpress.com.au>
2020-04-21 15:52:24 +1000

    minor changes

--- fabby-theme.js
+++ fabby-theme.js
@@ -3,7 +3,7 @@ import {createMuiTheme} from "@material-ui/core/styles";
 const theme = createMuiTheme({
     palette: {
         primary: {
-            main: '#94cca7',
+            main: '#9F0C0B',
         }
     }
 });
